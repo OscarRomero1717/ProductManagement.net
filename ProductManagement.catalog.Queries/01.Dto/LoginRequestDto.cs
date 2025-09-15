a19efa7f8b7d93fba65890bbfcbb136838b9465b
Oscar Romero <odromero1717@gmail.com>
2025-09-15 10:30:20 -0500

    Cambios Refactoring

--- ProductManagement.catalog.Queries/01.Dto/LoginRequestDto.cs
+++ ProductManagement.catalog.Queries/01.Dto/LoginRequestDto.cs
@@ -4,10 +4,10 @@ namespace Catalog.QueriesService._01.Dto
 {
     public class LoginRequestDto
     {
-        [Required(ErrorMessage = "Username is required")]
+        [Required()]
         public string Username { get; set; }
 
-        [Required(ErrorMessage = "Password is required")]
+        [Required()]
         public string Password { get; set; }
     }
 }
